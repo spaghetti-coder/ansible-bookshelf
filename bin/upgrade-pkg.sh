b2ae4d29964391ffe328c25e56354412141002eb
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-07-09 15:14:17 +0300

    upgrade packages script

---
+++ bin/upgrade-pkg.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+upgrade_pkg() {
+  local SELF_DIR; SELF_DIR="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
+  declare -a THE_CMD=(play -t upgradable-pkg)
+
+  local main_branch=master
+
+  main() {
+    cd -- "${SELF_DIR}/.." || return
+
+    git rev-parse --abbrev-ref HEAD | grep -qFx "${main_branch}" || {
+      echo "Can't perform upgrade while in not ${main_branch} branch" >&2
+      return 1
+    }
+
+    git status --porcelain | grep -q '.' && {
+      echo "Can't perform upgrade in not clean branch" >&2
+      return 1
+    }
+
+    # shellcheck disable=SC1091
+    . "${SELF_DIR}/play.sh"
+    "${THE_CMD[@]}" "${@}"
+  }
+
+  main "${@}"
+}
+
+(return 2>/dev/null) || upgrade_pkg "${@}"
--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -2,25 +2,13 @@
 
 upgrade() {
   local SELF_DIR; SELF_DIR="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
-  declare -a THE_CMD=(play -t upgradable)
-
-  local main_branch=master
+  declare -a THE_CMD=(upgrade_pkg -t upgradable)
 
   main() {
     cd -- "${SELF_DIR}/.." || return
 
-    git rev-parse --abbrev-ref HEAD | grep -qFx "${main_branch}" || {
-      echo "Can't perform upgrade while in not ${main_branch} branch" >&2
-      return 1
-    }
-
-    git status --porcelain | grep -q '.' && {
-      echo "Can't perform upgrade in not clean branch" >&2
-      return 1
-    }
-
     # shellcheck disable=SC1091
-    . "${SELF_DIR}/play.sh"
+    . "${SELF_DIR}/upgrade-pkg.sh"
     "${THE_CMD[@]}" "${@}"
   }
 
