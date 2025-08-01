d55187da83b2ae6cb0aaf0d62488768a73140588
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-08-01 16:50:26 +0300

    secure upgrade for code-server

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -7,6 +7,24 @@ upgrade() {
   main() {
     cd -- "${SELF_DIR}/.." || return
 
+    local force_code_server=false
+    if [ "${1}" = "-!" ]; then
+      force_code_server=true
+      shift
+    fi
+
+    if true \
+      && [ "${TERM_PROGRAM,,}" = vscode ] \
+      && [ "${NODE_EXEC_PATH}" = '/usr/lib/code-server/lib/node' ] \
+      && ! ${force_code_server:-false} \
+    ; then
+      printf -- '%s\n' \
+        "Upgrade from Code-Server is not allowed as update can break the connection." \
+        "Or pass '-!' flag as the very first argument to bypass it." \
+      >&2
+      return 1
+    fi
+
     # shellcheck disable=SC1091
     . "${SELF_DIR}/upgrade-pkg.sh"
     "${THE_CMD[@]}" "${@}"
