9af30928841889089ea013b58b86676b0182afb0
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-10-01 00:50:35 +0300

    cronmaster

---
+++ roles/service/cronmaster/files/demo-snippet.sh
@@ -0,0 +1,10 @@
+# @id: demo-snippet
+# @title: Demo snippet
+# @description: Demo snippet description
+# @category: Demos
+# @tags: demo1,demo2
+
+# id must be unique
+
+# Your bash script content goes here
+echo "Hello World!"
