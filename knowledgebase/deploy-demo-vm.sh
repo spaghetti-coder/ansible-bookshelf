51c066366c40db87d48ef4bdaa4a77fe5d20b359
Spaghetti <spaghetti.coder.ru@gmail.com>
2025-11-23 14:02:27 +0200

    make deploy vm profile required

--- knowledgebase/deploy-demo-vm.sh
+++ knowledgebase/deploy-demo-vm.sh
@@ -1,34 +1,51 @@
 #!/usr/bin/env bash
 
 #
-# NOTE:
-#   To add multiple profiles support rename deploy_ve_config function to
-#   for example deploy_ve_config_foo and create deploy_ve_config_bar with
-#   different configuration. This way the script will be able to handle
-#   both profiles.
-#   deploy_ve_config      <- correct
-#   deploy_ve_config_foo  <- correct
-#   deploy_ve_config_     <- will be ignored
+# README:
+#   - Each deploy_ve_config_* function serves as configuration profile
+#     for deployment
+#   - Profile functions can extend each other
+#   - By running `local my-profile` you load configuration from
+#     deploy_ve_config_my_profile function
 #
 
-# Default demo profile
-deploy_ve_config() {
+deploy_ve_config_ubuntu_2404_demo() {   # <- Demo profile
+  deploy_ve_TEMPLATE_CONFIG
+  REMOTE_EXPORTS+=(
+    # Current profile extends template => export the template to remote PVE
+    deploy_ve_TEMPLATE_CONFIG
+  )
+
+  # Default profile overrides
+  VE_ID=6001
+  VE_NAME=ubuntu2404-demo
+}
+
+# ~~~~~ BOILERPLATE ~~~~~
+
+mk_snapshot() {                         # <- Demo make snapshot callback
+  (set -x; qm snapshot "${VE_ID}" Init1 --description "Initial snapshot" >/dev/null)
+}
+
+# shellcheck disable=SC2016
+deploy_ve_TEMPLATE_CONFIG() {           # <- Demo profile template
   PVE_HOST=pve.local        # <- Only REQUIRED for remote deployment
   REMOTE_EXPORTS+=()        # <- Functions to be exported to remote PVE
 
-  VE_ID=6001                # <- Make sure >= 100
-  # VE_ID="$(pvesh get /cluster/nextid 2>/dev/null)"  # <- Not recommended, not idempotent
+  # VE_ID=auto generates new ID for each call,
+  # not idempotent => don't use unless you know ...
+  VE_ID=auto                # <- Ex.: 6001
   VE_NAME=ubuntu2404-demo1
   IMAGE_URL=https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
   STORAGE=local-lvm
 
   # ~~~~~ HARDWARE CONFIGURATION ~~~~~
-  CPU_TYPE=x86-64-v2-AES    # <- Works better than 'host'
+  CPU_TYPE=host       # <- Optionally 'x86-64-v2-AES'
   CORES=1
   DISK_SIZE=10G
   MEMORY=1024         # <- In MB
   NET_BRIDGE=vmbr0
-  NET_IP=dhcp         # 192.168.0.10/24,gw=192.168.0.1    # <- For static IP
+  NET_IP=dhcp         # '192.168.0.10/24,gw=192.168.0.1' or 'dhcp'
   AUTOSTART=0         # <- 0 or 1
 
   # ~~~~~ RUNTIME CONFIGURATION (optional) ~~~~~
@@ -39,9 +56,9 @@ deploy_ve_config() {
   #   openssl passwd -6 -stdin <<< PASSWORD_TEXT
   # Generate random password hash:
   #   tr -dc 'A-Za-z0-9' </dev/urandom | head -c 20 | openssl passwd -6 -stdin
-  CLOUD_PASSWORD="$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 20 | openssl passwd -6 -stdin)"
+  CLOUD_PASSWORD='changeme'
 
-  CUSTOMIZE_ARGS=(      # <- https://libguestfs.org/virt-customize.1.html
+  CUSTOMIZE_ARGS+=(    # <- https://libguestfs.org/virt-customize.1.html
     # Fix $CLOUD_USER HOME ownership (probably due to Virtiofs directories creation)
     --firstboot-command "chown ${CLOUD_USER}:${CLOUD_USER} /home/${CLOUD_USER}"
     # Guest agent + ansible target prereqs
@@ -54,31 +71,15 @@ deploy_ve_config() {
 
   # Mappings must be created in advance:
   #   Datacenter -> Directory Mappings
-  VIRTIOFS=(            # Format: 'MAPPING_ID:IN_VM_MOUNT_POINT'
+  VIRTIOFS+=(           # Format: 'MAPPING_ID:IN_VM_MOUNT_POINT'
     # 'my-mapping-id:/home/user/share'    # <- /home/user/share will be auto created
   )
 
-  ON_CREATED+=(         # Functions to be executed when the VM is created
+  ON_CREATED+=(
+    # Functions to be executed when the VM is created.
+    # Will be auto remote-exported
     mk_snapshot
   )
-}
-
-# One more demo profile
-deploy_ve_config_demo_2() {
-  deploy_ve_config
-
-  REMOTE_EXPORTS+=(
-    # Current profile depends on the default one => export default to remote PVE
-    deploy_ve_config
-  )
-
-  # Default profile overrides
-  VE_ID=6002
-  VE_NAME=ubuntu2404-demo2
-}
-
-mk_snapshot() {
-  (set -x; qm snapshot "${VE_ID}" Init1 --description "Initial snapshot" >/dev/null)
 }
 
 
@@ -93,8 +94,11 @@ mk_snapshot() {
 
 # shellcheck disable=SC2317,SC2329
 deploy_ve_core() (
+  local SELF_FUNC="${FUNCNAME[0]}"
   local SELF_SCRIPT=pve-deploy-vm.sh
   grep -q '.\+' -- "${0}" 2>/dev/null && SELF_SCRIPT="$(basename -- "${0}" 2>/dev/null)"
+
+  local PROFILE_FUNC_PREFIX=deploy_ve_config_
 
   local PVE_HOST
   local VE_ID \
@@ -145,20 +149,15 @@ deploy_ve_core() (
 
   profile_name_to_func() {
     local profile="${1}"
-    local prefix=deploy_ve_config
 
-    head -n 1 -- <<< "${profile}" | grep -qFxf <(list_profiles | sed -e 's/^*\s*//') || {
-      log_fatal "Profile not defined: '${profile}'"
+    head -n 1 -- <<< "${profile}" | grep -qFxf <(list_profiles) || {
+      log_fatal "No match for profile: '${profile}'"
       return 1
     }
 
-    local profile_func="${prefix}"
-    if [ -n "${profile}" ]; then
-      profile_func="$(
-        printf -- '%s\n' "${profile}" \
-        | sed -e 's/-/_/g' -e 's/^/'"${prefix}_"'/'
-      )"
-    fi
+    local profile_func; profile_func="${PROFILE_FUNC_PREFIX}$(
+      printf -- '%s\n' "${profile}" | sed -e 's/-/_/g'
+    )"
 
     declare -F -- "${profile_func}"
   }
@@ -166,6 +165,12 @@ deploy_ve_core() (
   check_pve_host() {
     pveversion &>/dev/null && return
     log_fatal "Not PVE host"
+    return 1
+  }
+
+  check_root() {
+    [ "$(id -u)" -eq 0 ] && return 0
+    log_fatal "Must be root"
     return 1
   }
 
@@ -180,6 +185,7 @@ deploy_ve_core() (
   # ~~~~~ LIB ~~~~~
 
   log_info()  { echo -e "${T_BOLD}${T_GREEN}INFO: ${T_RESET}${1}" >&2; }
+  log_warn()  { echo -e "${T_BOLD}${T_GREEN}WARN: ${T_RESET}${1}" >&2; }
   log_fatal() { echo -e "${T_RED}${T_BOLD}FATAL: ${T_RESET}${T_RED}${1}${T_RESET}" >&2; }
 
   # shellcheck disable=SC2001,SC2120
@@ -202,18 +208,24 @@ deploy_ve_core() (
 
     log_info "Remote deployment to ${PVE_HOST}"
 
-    local -a copy_func=(deploy_ve_core "${profile_func}" "${REMOTE_EXPORTS[@]}" "${ON_CREATED[@]}")
+    local -a copy_func=("${SELF_FUNC}" "${profile_func}" "${REMOTE_EXPORTS[@]}" "${ON_CREATED[@]}")
     ssh -- "${PVE_HOST}" "/bin/bash -c '
       $(
         set -o pipefail
         (set -x; declare -f -- "${copy_func[@]}") | escape_single_quote
       ) || exit
-      deploy_ve_core local $(escape_single_quote "${profile}")
+      ${SELF_FUNC} local $(escape_single_quote "${profile}")
     '"
     exit
   }
 
   deploy_local() {
+    [ "${VE_ID}" = auto ] && {
+      VE_ID="$(pvesh get /cluster/nextid)" || return
+      log_warn "VE_ID='auto' is not recommended!"
+      log_info "Generated VE_ID='${VE_ID}'"
+    }
+
     if qm config "${VE_ID}" &>/dev/null \
       || pct config "${VE_ID}" &>/dev/null \
     ; then
@@ -246,9 +258,6 @@ deploy_ve_core() (
 
       set -x
       virt-customize -q -a "${TMP_FILE}" \
-        --run-command 'systemd-machine-id-setup' \
-        --run-command 'echo -n > /etc/machine-id' \
-        --run-command 'ln -s -f /etc/machine-id /var/lib/dbus/machine-id' \
         "${extra_args[@]}" \
         "${CUSTOMIZE_ARGS[@]}" \
       >/dev/null
@@ -296,18 +305,20 @@ deploy_ve_core() (
         | sed -e 's/\(--cipassword[= ]\).\+/\1*****/g'
       ) 3>&1 1>&2 2>&3 || return
 
+      if [ -n "${SSH_PUB_KEY}" ]; then
+        printf -- '%s\n' "${SSH_PUB_KEY}" | (
+          set -x; qm set "${VE_ID}" --sshkeys <(cat) >/dev/null
+        ) || return
+      fi
+
       ( set -x
         qm cloudinit update "${VE_ID}" >/dev/null \
         && qm resize "${VE_ID}" scsi0 "${DISK_SIZE}" >/dev/null
       ) || return
     ) || return
 
-    if [ -n "${SSH_PUB_KEY}" ]; then
-      printf -- '%s\n' "${SSH_PUB_KEY}" | (set -x; qm set "${VE_ID}" --sshkeys <(cat) >/dev/null) || return
-    fi
-
     local hook; for hook in "${ON_CREATED[@]}"; do
-      log_info "Executing ${hook} hook"
+      log_info "Executing hook '${hook}'"
       "${hook}" || exit
     done
 
@@ -320,12 +331,10 @@ deploy_ve_core() (
   }
 
   list_profiles() {
-    declare -F | rev | cut -d ' ' -f1 | rev       `# <- Get funcs names` \
-    | sed -e 's/$/_/' | grep '^deploy_ve_config_' `# <- Get profilish ones` \
-    | sed -e 's/^deploy_ve_config_//' -e '/^_$/d' \
-          -e 's/_$//' -e 's/_/-/g'                `# <- Get profile names` \
-    | sort \
-    | sed -e 's/^/* /'
+    declare -F | rev | cut -d ' ' -f1 | rev                 `# <- Get funcs names` \
+    | grep "^${PROFILE_FUNC_PREFIX}"'.\+'                   `# <- Get profilish ones` \
+    | sed -e 's/^'"${PROFILE_FUNC_PREFIX}"'//' -e 's/_/-/g' `# <- Get profile names` \
+    | sort
   }
 
   show_profile() {
@@ -338,9 +347,11 @@ deploy_ve_core() (
     echo "
       USAGE:
       ~~~~~~
-      ${SELF_SCRIPT} local|remote [PROFILE]   # <- Deploy PVE VM
-      ${SELF_SCRIPT} profiles                 # <- List all profiles
-      ${SELF_SCRIPT} profile [PROFILE]        # <- View profile
+      ${SELF_SCRIPT} --help|--usage|--demo  # <- Helps
+     ,
+      ${SELF_SCRIPT} profiles               # <- List available profiles
+      ${SELF_SCRIPT} profile PROFILE        # <- View PROFILE
+      ${SELF_SCRIPT} local|remote PROFILE   # <- Deploy PROFILE to PVE VM
     " | sed -e 's/^\s*//' -e 's/\s*$//' -e '/^$/d' -e 's/^,//'
   }
 
@@ -350,16 +361,10 @@ deploy_ve_core() (
       ~~~~~
       # Deployment target must be PVE
       # Assuming 2 config functions present:
-      #   deploy_ve_config() { \`# Default profile\`; }
-      #   deploy_ve_config_nas_server() { \`# NAS server profile\`; }
-      ${SELF_SCRIPT} local      # <- Default profile targets current machine
-      ${SELF_SCRIPT} local ''   # <- Same as previous
-      ${SELF_SCRIPT} remote     # <- Same, but targets \$PVE_HOST machine via SSH
-      ${SELF_SCRIPT} local nas-server   # <- NAS server profile. NOTE: '_' -> '-'
-     ,
-      # View profile:
-      ${SELF_SCRIPT} profile              # <- View default profile
-      ${SELF_SCRIPT} profile nas-server   # <- View NAS server profile
+      #   ${PROFILE_FUNC_PREFIX}dev_server() { \`# ... Dev server profile config ...\`; }
+      #   ${PROFILE_FUNC_PREFIX}nas_server() { \`# ... NAS server profile config ...\`; }
+      ${SELF_SCRIPT} local nas-server   # <- Targets the current machine
+      ${SELF_SCRIPT} remote dev-server  # <- Targets \$PVE_HOST machine via SSH
     " | sed -e 's/^\s*//' -e 's/\s*$//' -e '/^$/d' -e 's/^,//'
   }
 
@@ -381,8 +386,9 @@ deploy_ve_core() (
 
     local profile_func; profile_func="$(profile_name_to_func "${@:2}")" || exit
     "${profile_func}"
-    trap_remote "${@}"  # || exit # <- It handles all exits
+    trap_remote "${@}"  # || exit # <- exit is handled by trap_remote
     check_pve_host || exit
+    check_root || exit
     "${CMD_MAP[${1}]}" "${@:2}" || exit
   }
 
